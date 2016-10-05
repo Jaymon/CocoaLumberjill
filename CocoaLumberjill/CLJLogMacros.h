1ee821002545b59dfc4a4dfdf55e14973a78683e
Jay Marcyes <jay@marcyes.com>
2016-10-05 15:05:22 -0700

    adds some more macros

--- CocoaLumberjill/CLJLogMacros.h
+++ CocoaLumberjill/CLJLogMacros.h
@@ -2,8 +2,18 @@
 #import "DDLog.h"
 
 
+// DEPRECATED 10-2-2016, use the DDLogMethodDebug macro, turns out I wanted to do the
+// same thing on verbose also, so this has been expanded into 2 different macros
+#define DDLogMethod() LOG_MAYBE(LOG_ASYNC_ENABLED, LOG_LEVEL_DEF, DDLogFlagDebug, 0, nil, __PRETTY_FUNCTION__, @"%@(%p).%@", NSStringFromClass([self class]), self, NSStringFromSelector(_cmd))
+
+
 /**
  This just logs the Class.method as DEBUG log level, just something I do often enough
  */
-#define DDLogMethod() LOG_MAYBE(LOG_ASYNC_ENABLED, LOG_LEVEL_DEF, DDLogFlagDebug, 0, nil, __PRETTY_FUNCTION__, @"%@(%p).%@", NSStringFromClass([self class]), self, NSStringFromSelector(_cmd))
+#define DDLogMethodDebug() LOG_MAYBE(LOG_ASYNC_ENABLED, LOG_LEVEL_DEF, DDLogFlagDebug, 0, nil, __PRETTY_FUNCTION__, @"%@(%p).%@", NSStringFromClass([self class]), self, NSStringFromSelector(_cmd))
 
+
+/**
+ This just logs the Class.method as VERBOSE log level, just something I do often enough
+ */
+#define DDLogMethodVerbose() LOG_MAYBE(LOG_ASYNC_ENABLED, LOG_LEVEL_DEF, DDLogFlagVerbose, 0, nil, __PRETTY_FUNCTION__, @"%@(%p).%@", NSStringFromClass([self class]), self, NSStringFromSelector(_cmd))
